c904ee0c5aa5eeb807c0b9f80d2e7ee625bed1cc
markgoho <markgoho@gmail.com>
2017-10-09 21:18:17 -0400

    Add basic mvc to js file

--- index.js
+++ index.js
@@ -1,62 +1,80 @@
-pictures = [
-  {
-    imgPath: 'photos/01.jpg',
-    title: 'Hay Bales',
-    caption:
-      'I love hay bales. Took this snap on a drive through the countryside past some straw fields.'
+const model = {
+  currentPicture: null,
+
+  pictures: [
+    {
+      imgPath: 'photos/01.jpg',
+      title: 'Hay Bales',
+      caption:
+        'I love hay bales. Took this snap on a drive through the countryside past some straw fields.'
+    },
+    {
+      imgPath: 'photos/01.jpg',
+      title: 'Hay Bales',
+      caption:
+        'I love hay bales. Took this snap on a drive through the countryside past some straw fields.'
+    },
+    {
+      imgPath: 'photos/01.jpg',
+      title: 'Hay Bales',
+      caption:
+        'I love hay bales. Took this snap on a drive through the countryside past some straw fields.'
+    },
+    {
+      imgPath: 'photos/01.jpg',
+      title: 'Hay Bales',
+      caption:
+        'I love hay bales. Took this snap on a drive through the countryside past some straw fields.'
+    },
+    {
+      imgPath: 'photos/01.jpg',
+      title: 'Hay Bales',
+      caption:
+        'I love hay bales. Took this snap on a drive through the countryside past some straw fields.'
+    },
+    {
+      imgPath: 'photos/01.jpg',
+      title: 'Hay Bales',
+      caption:
+        'I love hay bales. Took this snap on a drive through the countryside past some straw fields.'
+    },
+    {
+      imgPath: 'photos/01.jpg',
+      title: 'Hay Bales',
+      caption:
+        'I love hay bales. Took this snap on a drive through the countryside past some straw fields.'
+    },
+    {
+      imgPath: 'photos/01.jpg',
+      title: 'Hay Bales',
+      caption:
+        'I love hay bales. Took this snap on a drive through the countryside past some straw fields.'
+    },
+    {
+      imgPath: 'photos/01.jpg',
+      title: 'Hay Bales',
+      caption:
+        'I love hay bales. Took this snap on a drive through the countryside past some straw fields.'
+    },
+    {
+      imgPath: 'photos/01.jpg',
+      title: 'Hay Bales',
+      caption:
+        'I love hay bales. Took this snap on a drive through the countryside past some straw fields.'
+    }
+  ]
+};
+
+const controller = {
+  init() {}
+};
+
+// View(s)
+const galleryView = {
+  init() {
+    // store pointers to DOM elements for later access
+
+    this.render();
   },
-  {
-    imgPath: 'photos/01.jpg',
-    title: 'Hay Bales',
-    caption:
-      'I love hay bales. Took this snap on a drive through the countryside past some straw fields.'
-  },
-  {
-    imgPath: 'photos/01.jpg',
-    title: 'Hay Bales',
-    caption:
-      'I love hay bales. Took this snap on a drive through the countryside past some straw fields.'
-  },
-  {
-    imgPath: 'photos/01.jpg',
-    title: 'Hay Bales',
-    caption:
-      'I love hay bales. Took this snap on a drive through the countryside past some straw fields.'
-  },
-  {
-    imgPath: 'photos/01.jpg',
-    title: 'Hay Bales',
-    caption:
-      'I love hay bales. Took this snap on a drive through the countryside past some straw fields.'
-  },
-  {
-    imgPath: 'photos/01.jpg',
-    title: 'Hay Bales',
-    caption:
-      'I love hay bales. Took this snap on a drive through the countryside past some straw fields.'
-  },
-  {
-    imgPath: 'photos/01.jpg',
-    title: 'Hay Bales',
-    caption:
-      'I love hay bales. Took this snap on a drive through the countryside past some straw fields.'
-  },
-  {
-    imgPath: 'photos/01.jpg',
-    title: 'Hay Bales',
-    caption:
-      'I love hay bales. Took this snap on a drive through the countryside past some straw fields.'
-  },
-  {
-    imgPath: 'photos/01.jpg',
-    title: 'Hay Bales',
-    caption:
-      'I love hay bales. Took this snap on a drive through the countryside past some straw fields.'
-  },
-  {
-    imgPath: 'photos/01.jpg',
-    title: 'Hay Bales',
-    caption:
-      'I love hay bales. Took this snap on a drive through the countryside past some straw fields.'
-  }
-];
+  render() {}
+};
