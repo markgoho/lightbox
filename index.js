1362a3f5d4069a49d5299aeaa8b42017f9f0489a
Mark Goho <markgoho@gmail.com>
2017-10-11 16:12:54 -0400

    basic implementation of lightbox

--- index.js
+++ index.js
@@ -1,63 +1,71 @@
 const model = {
-  currentPicture: null,
-
   pictures: [
     {
       imgPath: 'photos/01.jpg',
+      thumbPath: 'photos/thumbnails/01.jpg',
       title: 'Hay Bales',
       caption:
         'I love hay bales. Took this snap on a drive through the countryside past some straw fields.'
     },
     {
-      imgPath: 'photos/01.jpg',
+      imgPath: 'photos/02.jpg',
+      thumbPath: 'photos/thumbnails/02.jpg',
       title: 'Hay Bales',
       caption:
         'I love hay bales. Took this snap on a drive through the countryside past some straw fields.'
     },
     {
-      imgPath: 'photos/01.jpg',
+      imgPath: 'photos/03.jpg',
+      thumbPath: 'photos/thumbnails/03.jpg',
       title: 'Hay Bales',
       caption:
         'I love hay bales. Took this snap on a drive through the countryside past some straw fields.'
     },
     {
-      imgPath: 'photos/01.jpg',
+      imgPath: 'photos/04.jpg',
+      thumbPath: 'photos/thumbnails/04.jpg',
       title: 'Hay Bales',
       caption:
         'I love hay bales. Took this snap on a drive through the countryside past some straw fields.'
     },
     {
-      imgPath: 'photos/01.jpg',
+      imgPath: 'photos/05.jpg',
+      thumbPath: 'photos/thumbnails/05.jpg',
       title: 'Hay Bales',
       caption:
         'I love hay bales. Took this snap on a drive through the countryside past some straw fields.'
     },
     {
-      imgPath: 'photos/01.jpg',
+      imgPath: 'photos/06.jpg',
+      thumbPath: 'photos/thumbnails/06.jpg',
       title: 'Hay Bales',
       caption:
         'I love hay bales. Took this snap on a drive through the countryside past some straw fields.'
     },
     {
-      imgPath: 'photos/01.jpg',
+      imgPath: 'photos/07.jpg',
+      thumbPath: 'photos/thumbnails/07.jpg',
       title: 'Hay Bales',
       caption:
         'I love hay bales. Took this snap on a drive through the countryside past some straw fields.'
     },
     {
-      imgPath: 'photos/01.jpg',
+      imgPath: 'photos/08.jpg',
+      thumbPath: 'photos/thumbnails/08.jpg',
       title: 'Hay Bales',
       caption:
         'I love hay bales. Took this snap on a drive through the countryside past some straw fields.'
     },
     {
-      imgPath: 'photos/01.jpg',
+      imgPath: 'photos/09.jpg',
+      thumbPath: 'photos/thumbnails/09.jpg',
       title: 'Hay Bales',
       caption:
         'I love hay bales. Took this snap on a drive through the countryside past some straw fields.'
     },
     {
-      imgPath: 'photos/01.jpg',
+      imgPath: 'photos/10.jpg',
+      thumbPath: 'photos/thumbnails/10.jpg',
       title: 'Hay Bales',
       caption:
         'I love hay bales. Took this snap on a drive through the countryside past some straw fields.'
@@ -66,15 +74,65 @@ const model = {
 };
 
 const controller = {
-  init() {}
+  init() {
+    galleryView.init();
+    lightboxView.init();
+    searchboxView.init();
+  },
+
+  get images() {
+    return model.pictures;
+  }
 };
 
 // View(s)
 const galleryView = {
   init() {
     // store pointers to DOM elements for later access
-
+    this.gallery = document.querySelector('.gallery');
     this.render();
   },
-  render() {}
+  render() {
+    controller.images.forEach(image => {
+      const a = document.createElement('a');
+      a.href = image.imgPath;
+
+      const img = document.createElement('img');
+      img.src = image.thumbPath;
+
+      a.addEventListener('click', e => {
+        e.preventDefault();
+        lightboxView.render(image);
+      });
+
+      a.appendChild(img);
+
+      this.gallery.appendChild(a);
+    });
+  }
 };
+
+const lightboxView = {
+  init() {
+    this.lightbox = document.querySelector('.lightbox');
+    this.previous = document.querySelector('.previous');
+    this.next = document.querySelector('.next');
+    this.lbImg = document.querySelector('.lb-image > img');
+    this.caption = document.querySelector('caption');
+
+    this.lightbox.addEventListener('click', () => this.destroy());
+  },
+  render(image) {
+    this.lbImg.src = image.imgPath;
+    this.lightbox.classList.remove('hidden');
+  },
+  destroy() {
+    this.lightbox.classList.add('hidden');
+  }
+};
+
+const searchboxView = {
+  init() {}
+};
+
+controller.init();
